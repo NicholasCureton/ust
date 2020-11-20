650cdf98dfdab52852ce9d65bcb2f086d60569e5
NicholasCureton <NicholasMCureton@gmail.com>
2020-11-21 05:05:29 +0630

    First Commit

---
+++ bs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A sample Bash script, type .\bs.sh in terminal to run script
+# or type $bash myscript.sh 
+# Dont' fucking change the first line of bash code. It's called SheBang or something like that
+browser-sync --server --files "*.html, css/*.css, js/*.js"
